392c1701c1a7d183a39995d46d6ff78f20c92de4
pjhyng0125 <pjhyng0125@naver.com>
2022-04-02 12:38:30 +0900

    [ch.06] Spring DI 통한 객체지향 다형성 활용, 순수 JDBC 구현

--- hello-spring/src/main/java/com/pjh/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/pjh/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.pjh.hello.hellospring;
 
+import com.pjh.hello.hellospring.repository.JdbcMemberRepository;
 import com.pjh.hello.hellospring.repository.MemberRepository;
 import com.pjh.hello.hellospring.repository.MemoryMemberRepository;
 import com.pjh.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
