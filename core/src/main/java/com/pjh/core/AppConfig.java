6438483b1967e5d592bc3230d50459f38489c0d4
pjhyng0125 <pjhyng0125@naver.com>
2022-05-03 00:20:19 +0900

    [ch.03] 핵심원리 기본 - AppConfig 리펙터링, 중복 제거

--- core/src/main/java/com/pjh/core/AppConfig.java
+++ core/src/main/java/com/pjh/core/AppConfig.java
@@ -1,6 +1,9 @@
 package com.pjh.core;
 
+import com.pjh.core.discount.DiscountPolicy;
 import com.pjh.core.discount.FixDiscountPolicy;
+import com.pjh.core.discount.RateDiscountPolicy;
+import com.pjh.core.member.MemberRepository;
 import com.pjh.core.member.MemberService;
 import com.pjh.core.member.MemberServiceImpl;
 import com.pjh.core.member.MemoryMemberRepository;
@@ -9,10 +12,19 @@ import com.pjh.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+    }
+
+    private MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy getDiscountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- core/src/test/java/com/pjh/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/com/pjh/core/discount/RateDiscountPolicyTest.java
@@ -16,12 +16,12 @@ class RateDiscountPolicyTest {
     void vip_o() {
         Member memberVIP = new Member(1L, "memberVIP", Grade.VIP);
 
-        int discount = discountPolicy.discount(memberVIP, 10000);
-        assertEquals(1000, discount);
+        int discount = discountPolicy.discount(memberVIP, 20000);
+        assertEquals(2000, discount);
     }
 
     @Test
-    @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다")
+    @DisplayName("BASIC은 10% 할인이 적용되지 않아야 한다")
     void vip_x() {
         Member memberBASIC = new Member(1L, "memberBASIC", Grade.BASIC);
 
