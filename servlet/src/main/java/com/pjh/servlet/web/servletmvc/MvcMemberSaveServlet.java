9401a6023ed3d228bda5587877640da3b0ad0aca
pjhyng0125 <pjhyng0125@naver.com>
2022-06-11 16:40:54 +0900

    [ch.03] 스프링 MVC 1편 - servelt, jsp, MVC 패턴 Member 샘플 소스 추가

---
+++ servlet/src/main/java/com/pjh/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,33 @@
+package com.pjh.servlet.web.servletmvc;
+
+import com.pjh.servlet.basic.domain.member.Member;
+import com.pjh.servlet.basic.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, res);
+    }
+}
